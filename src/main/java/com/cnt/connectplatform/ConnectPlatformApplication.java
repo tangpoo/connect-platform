22f8e0fb0d76fcef6caa9be477f3985835c3455c
tangpoo <lhinav3@gmail.com>
2025-07-01 15:58:21 +0900

    first commit

---
+++ src/main/java/com/cnt/connectplatform/ConnectPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.cnt.connectplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cnt/connectplatform/ConnectPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnt.connectplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
