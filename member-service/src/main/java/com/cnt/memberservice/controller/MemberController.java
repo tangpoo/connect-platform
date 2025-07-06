36dee2e60f8005adee89caf8919e073dcef4e549
tangpoo <lhinav3@gmail.com>
2025-07-06 11:29:41 +0900

    #14 Feat: Swagger-ui 적용

--- member-service/src/main/java/com/cnt/memberservice/controller/MemberController.java
+++ member-service/src/main/java/com/cnt/memberservice/controller/MemberController.java
@@ -2,6 +2,8 @@ package com.cnt.memberservice.controller;
 
 import com.cnt.memberservice.dto.MemberDto;
 import com.cnt.memberservice.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -15,11 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/members")
+@Tag(name = "Member API")
 public class MemberController {
 
     private final MemberService memberService;
 
     @GetMapping
+    @Operation(summary = "회원 목록 조회")
     public Page<MemberDto> getMembers(
         @RequestParam(defaultValue = "createdAt") String sortBy,
         @RequestParam(defaultValue = "0") int page,
@@ -29,6 +33,7 @@ public class MemberController {
     }
 
     @PostMapping("/{id}/views")
+    @Operation(summary = "상세 조회수 증가")
     public ResponseEntity<Void> increaseViewCount(@PathVariable Long id) {
         memberService.increaseViewCount(id);
         return ResponseEntity.ok().build();
---
+++ payment-service/src/main/java/com/cnt/paymentservice/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package com.cnt.paymentservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(
+            new Info().title("OpenAPI.").version("1.0").description("swagger-ui 화면입니다."));
+    }
+}
